ac57ec9f9d7b0bab00905173fdb68cdabaf94b92
JohanP <johanpieterscpt@gmail.com>
2025-09-16 12:54:33 +0200

    initial setup

---
+++ main.py
@@ -0,0 +1,10 @@
+import pygame
+from constants import *
+
+def main():
+    print("Starting Asteroids!")
+    print(F"Screen width:{SCREEN_WIDTH:>5}")
+    print(F"Screen height:{SCREEN_HEIGHT:>4}")
+
+if __name__ == "__main__":
+    main()
