17572845e26c69f8f67ad853737ae84bf0639426
JohanP <johanpieterscpt@gmail.com>
2025-09-19 21:39:24 +0200

    Added a clock, tick and delta time

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ from constants import *
 
 def main():
     pygame.init()
-    print("Starting Asteroids!")
+    #print("Starting Asteroids!")
     #print(F"Screen width:{SCREEN_WIDTH:>5}")
     #print(F"Screen height:{SCREEN_HEIGHT:>4}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     black = (0, 0, 0)
+    gameclock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -16,8 +18,8 @@ def main():
                 return
                
         screen.fill(black)
-
         pygame.display.flip()
+        dt = gameclock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
